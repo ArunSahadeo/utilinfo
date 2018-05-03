c89b182cdfdd9412d486bb96fb7022d7a8898fbe
Arun Sahadeo <arunjamessahadeo@gmail.com>
2018-05-03 01:06:09 +0100

    Format MB to GB

--- lib/linux_utilinfo/memory_methods.rb
+++ lib/linux_utilinfo/memory_methods.rb
@@ -1,8 +1,11 @@
 module LinuxUtilinfo
 	module MemoryMethods
 		def get_total_memory
-				memory_total_in_kb = `cat /proc/meminfo | grep -i 'MemTotal' | awk '{print $2 " " $3}'` 
-				return memory_total_in_kb
+				memory_total_in_kb = `cat /proc/meminfo | grep -i 'MemTotal' | awk '{print $2}'` 
+				memory_total_in_kb /= 1000
+				memory_total_in_kb /= 1000
+				memory_total_in_gb = "#{memory_total_in_gb}GB"
+				return memory_total_in_gb
 		end
 	end
 end
