20c3f17d6818ca37bfa165d4c1b7285d5c4ef8c1
Arun Sahadeo <arunjamessahadeo@gmail.com>
2018-05-07 12:45:42 +0100

    Bump version and fix memory method

--- lib/linux_utilinfo/memory_methods.rb
+++ lib/linux_utilinfo/memory_methods.rb
@@ -3,7 +3,7 @@ module LinuxUtilinfo
 		def get_total_memory
 				memory_total_in_kb = `cat /proc/meminfo | grep -i 'MemTotal' | awk '{print $2}'`.to_i 
 				memory_total_in_kb /= (1000**2)
-				memory_total_in_gb = "#{memory_total_in_gb}GB"
+				memory_total_in_gb = "#{memory_total_in_kb}GB"
 				return memory_total_in_gb
 		end
 	end
