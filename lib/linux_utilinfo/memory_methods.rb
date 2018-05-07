f83e6e7ba5fd4ab2531c2d1781ec55feed55b461
Arun Sahadeo <arunjamessahadeo@gmail.com>
2018-05-07 12:33:54 +0100

    Update memory calculation

--- lib/linux_utilinfo/memory_methods.rb
+++ lib/linux_utilinfo/memory_methods.rb
@@ -2,8 +2,7 @@ module LinuxUtilinfo
 	module MemoryMethods
 		def get_total_memory
 				memory_total_in_kb = `cat /proc/meminfo | grep -i 'MemTotal' | awk '{print $2}'` 
-				memory_total_in_kb /= 1000
-				memory_total_in_kb /= 1000
+				memory_total_in_kb /= (1000**2)
 				memory_total_in_gb = "#{memory_total_in_gb}GB"
 				return memory_total_in_gb
 		end
